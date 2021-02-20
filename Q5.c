f81cca24911ea4c017c2b88d0307318335a3311d
sutapa2002 <78480546+sutapa2002@users.noreply.github.com>
2021-02-20 20:26:06 +0530

    Add files via upload

---
+++ Q5.c
@@ -0,0 +1,30 @@
+
+   #include<stdio.h>
+int main()
+{
+    
+    int i,j,size;
+ printf("Enter size : ");
+    scanf("%d",&size);
+    
+    
+    for(i=0; i < size ;i++)
+    {
+        
+        for(j=0; j < size ;j++)
+        {
+            
+            if(i==j || j==(size-i-1))
+            {
+                printf("*");
+            }
+            else
+            {
+                printf(" ");
+            }
+        }
+        
+        printf("\n");
+    }
+    return 0;
+}
